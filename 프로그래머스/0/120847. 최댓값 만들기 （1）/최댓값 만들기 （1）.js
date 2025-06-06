f5435c91ba62f77ae959c1d8ba4ddde8e3812953
lucy-kim04 <a01025105259@gmail.com>
2025-06-06 11:03:41 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const answer = []
+    for(let i = 0; i<numbers.length;i++) {
+        for(let j = 0; j<i;j++) {
+            answer.push(numbers[i]*numbers[j])
+        }
+    }
+    return Math.max(...answer)
+}
