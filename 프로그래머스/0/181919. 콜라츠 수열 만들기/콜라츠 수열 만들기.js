6244ac449bb7d0f3cbe5c166c5718e4dc9edcc90
lucy-kim04 <a01025105259@gmail.com>
2025-06-13 13:37:45 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = [n];
+    while(n!==1) {
+        if(n%2===0) {
+            answer.push(n/2);
+            n=n/2
+        } else {
+            answer.push(3*n+1);
+            n=(3*n+1)
+        }
+        
+    }
+    return answer
+}
