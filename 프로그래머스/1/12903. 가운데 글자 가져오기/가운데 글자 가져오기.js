fc44e68335f3dcbd08ed990368bf1719b93a9b1e
lucy-kim04 <a01025105259@gmail.com>
2025-06-15 20:47:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const mid = Math.floor(s.length / 2);
+    if (s.length % 2 === 0) {
+        return s.slice(mid - 1, mid + 1);
+    } else {
+        return s.slice(mid, mid + 1);
+    }
+}
